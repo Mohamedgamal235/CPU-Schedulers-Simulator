cf785ee7a2da3f0e954f317601561a0b419444b0
Basma-mmdouh25 <147372400+Basma-mmdouh25@users.noreply.github.com>
2024-12-06 03:15:39 +0200

    Update Process.java

--- Classes/Process.java
+++ Classes/Process.java
@@ -1,23 +1,96 @@
 package scheduling;
 
-class Process {
-    private String name ;
-    private int priority;
+// class Process {
+//     private String name ;
+//     private int priority;
+//     private int burstTime;
+//     private int arrivalTime;
+//     private  int remainingBurstTime;
+//     private int waitingTime;
+//     private int quantum ;
+//     int completionTime;
+//     int turnaroundTime;
+//     int id;
+
+//     public Process(String name, int burstTime, int arrivalTime, int priority, int quantum) {
+//         this.name = name;
+//         this.burstTime = burstTime;
+//         this.arrivalTime = arrivalTime;
+//         this.priority = priority;
+//         this.quantum = quantum;
+//         this.id=id;
+//     }
+// }
+
+public class Process {
+    private String name;
     private int burstTime;
     private int arrivalTime;
-    private  int remainingBurstTime;
+    private int remainingBurstTime;
     private int waitingTime;
+    private int completionTime;
+    private int turnaroundTime;
+    private boolean isCompleted;
+    private int priority;
     private int quantum ;
-    int completionTime;
-    int turnaroundTime;
     int id;
-
-    public Process(String name, int burstTime, int arrivalTime, int priority, int quantum) {
+    public Process(String name, int burstTime, int arrivalTime,int priority,int quantum) {
         this.name = name;
         this.burstTime = burstTime;
         this.arrivalTime = arrivalTime;
-        this.priority = priority;
-        this.quantum = quantum;
-        this.id=id;
+        this.remainingBurstTime = burstTime;
+        this.isCompleted = false;
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getRemainingBurstTime() {
+        return remainingBurstTime;
+    }
+
+    public void setRemainingBurstTime(int remainingBurstTime) {
+        this.remainingBurstTime = remainingBurstTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
     }
 }
